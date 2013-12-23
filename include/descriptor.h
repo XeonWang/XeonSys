f393a52ffadd5a991c77d39e3a25438e3e661003
XeonWang <wangzhiqiang1026@gmail.com>
2013-12-23 23:02:48 +0800

    gdt load

--- include/descriptor.h
+++ include/descriptor.h
@@ -1,8 +1,21 @@
 #ifndef _DESC_H
 #define _DESC_H
 
-struct desc_struct {
-	unsigned long a, b;
-};
+#define _LGDT(gdt) __asm__( \
+	"lgdt gdt_desc\n\t" \
+	"gdt_desc:\n\t" \
+	".word 256*8-1 \n\t"\
+	".long %0" \
+	::"m" (*gdt) \
+)
 
 #endif
+
+struct global_desc {
+	char limit_0_7, limit_8_15;
+	char base_0_7, base_8_15;
+	char base_16_23;
+	char type; //this type include type and P, DPL...
+	char limit_16_19; //this field include G, AVL, and limit bit 16-19...
+	char base_24_31; 
+};
---
+++ kernel/main.c
@@ -0,0 +1,50 @@
+#include <descriptor.h>
+
+struct global_desc * gdt[256];
+
+void set_global_seg()
+{
+	gdt[0]->limit_0_7 = 0x0;
+	gdt[0]->limit_8_15 = 0x0;
+	gdt[0]->base_0_7 = 0x0;
+	gdt[0]->base_8_15 = 0x0;
+	gdt[0]->base_16_23 = 0x0;
+	gdt[0]->type = 0x0; 
+	gdt[0]->limit_16_19 = 0x0; 
+	gdt[0]->base_24_31 = 0x0;
+
+	gdt[1]->limit_0_7 = 0xFF;
+	gdt[1]->limit_8_15 = 0xFF;
+	gdt[1]->base_0_7 = 0x0;
+	gdt[1]->base_8_15 = 0x0;
+	gdt[1]->base_16_23 = 0x0;
+	gdt[1]->type = 0x9A; //P=1, DPL=0, S=1, type=1010
+	gdt[1]->limit_16_19 = 0xCF; //G=1(4k), D=1(32bit)
+	gdt[1]->base_24_31 = 0x0;
+
+	gdt[2]->limit_0_7 = 0xFF;
+	gdt[2]->limit_8_15 = 0xFF;
+	gdt[2]->base_0_7 = 0x0;
+	gdt[2]->base_8_15 = 0x0;
+	gdt[2]->base_16_23 = 0x0;
+	gdt[2]->type = 0x92; //P=1, DPL=0, S=1, type=1010
+	gdt[2]->limit_16_19 = 0xCF; //G=1(4k), D=1(32bit)
+	gdt[2]->base_24_31 = 0x0;
+}
+
+void test_C()
+{
+	char *video_memory = (char*) 0xB8000;
+	*video_memory = 'H';
+	*(video_memory+2) = 'e';
+	*(video_memory+4) = 'l';
+	*(video_memory+6) = 'l';
+	*(video_memory+8) = 'o';
+}
+
+void main() {
+	test_C();
+	
+	set_global_seg();
+	_LGDT(gdt);
+}
