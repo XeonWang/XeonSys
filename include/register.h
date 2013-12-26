1c1e1fc9536dda98c14394180cc9db2ca1aaa556
XeonWang <wangzhiqiang1026@gmail.com>
2013-12-26 23:49:24 +0800

    add change to user mode, have not call

--- include/register.h
+++ include/register.h
@@ -20,6 +20,23 @@
 	); \
 }
 
+#define change_to_user_mode() { \
+	__asm__("movl %%esp, %%eax \n\t" \
+		"pushl $0x17 \n\t " \
+		"pushl %%eax \n\t " \
+		"pushfl \n\t " \
+		"pushl $0x0f \n\t " \
+		"pushl $1f \n\t " \
+		"iret \n\t" \
+		"1: \n\t" \
+		"movl $0x17, %%eax \n\t" \
+		"movw %%ax, %%ds\n\t" \
+		"movw %%ax, %%es\n\t" \
+		"movw %%ax, %%fs\n\t" \
+		"movw %%ax, %%gs\n\t" \
+		:::"ax"); \
+}
+
 #define _STI() __asm__("sti");
 
 #endif
