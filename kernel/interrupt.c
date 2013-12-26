3bba0c94adff2f160457ed1c9a78af28a6d8e78f
Xeon <xeon@xeon-VirtualBox.(none)>
2013-12-26 11:11:48 +0800

    extract the iret out

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -1,8 +1,9 @@
+#define _IRET() __asm__("leave \n\t iret")
 void clock_interrupt() {
 	char *video_memory = (char*) 0xB8000;
 	*video_memory = (*video_memory) + 1;
 	*video_memory = (*video_memory) + 1;
 	__asm__("outb %%al, $0x20"::"a" (0x20));
 //	__asm__("outb %%al, $0xA0"::"a" (0x20));
-	__asm__("leave \n\t iret");
+	_IRET();
 }
